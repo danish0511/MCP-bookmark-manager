883356502b3d4ea9dda390d75532f35d9f79b4a2
danish0511 <dsdd0511@gmail.com>
2025-09-24 10:22:31 +0530

    update

---
+++ src/hooks/useBookmarks.ts
@@ -0,0 +1,87 @@
+"use client"
+
+import { useEffect, useState } from "react"
+import { Bookmark, CreateBookmarkData } from "@/types/bookmark"
+
+
+export function useBookmarks() {
+    const [bookmarks, setBookmarks] = useState<Bookmark[]>([])
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState<string | null>(null)
+
+    useEffect(() => {
+        fetchBookmarks();
+    }, [])
+
+    const fetchBookmarks = async () => {
+        try {
+            setError(null)
+            const response = await fetch("/api/bookmarks")
+
+            if(!response.ok) {
+                throw new Error("failed to get bookmark")
+            }
+
+            const data = await response.json()
+            setBookmarks(data)
+        }
+        catch(err) {
+            setError(err instanceof Error ? err.message : "An error occured")
+        }
+        finally {
+            setLoading(false)
+        }
+    }
+
+    const addBookmark = async (data: CreateBookmarkData) => {
+        try {
+            setError(null)
+            const response = await fetch("/api/bookmarks", {
+                method: "POST",
+                headers: {
+                    "Content-Tyle": "application/json",
+                },
+                body: JSON.stringify(data),
+            })
+
+            if(!response.ok) {
+                throw new Error("Failed to add bookmark")
+            }
+
+            const newBookmark = await response.json()
+            setBookmarks((prev) => [newBookmark, ...prev])
+            return newBookmark
+        }
+        catch(err) {
+            const errorMessage = err instanceof Error ? err.message : "An error occured"
+            setError(errorMessage)
+        }
+    }
+
+    const   deleteBookmark = async(id: string) => {
+        try {
+            setError(null)
+            const response = await fetch(`/api/bookmarks/${id}`, {method: "DELETE"})
+
+            if(!response.ok) {
+                throw new Error("Failed to delete bookmark")
+            }
+
+            setBookmarks((prev) => prev.filter((bookmark) => bookmark.id !== id))
+        }
+        catch(err) {
+            const errorMessage = err instanceof Error ? err.message : "An error occured"
+            setError(errorMessage)
+        }
+    }
+
+    return {
+        bookmarks,
+        loading,
+        error,
+        addBookmark,
+        deleteBookmark,
+        refetch: fetchBookmarks,
+    }
+}
+
